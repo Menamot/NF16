b0f8385fae1a1c3e9376d8c64a4e1686bc313de8
partherope <58352330+partherope@users.noreply.github.com>
2022-10-21 22:17:54 +0200

    Update main.c

--- TP3/main.c
+++ TP3/main.c
@@ -1,3 +1,11 @@
 //
 // Created by 陈文龙 on 2022/10/21.
 //
+#include <stdio.h>
+#include "tp3.h"
+int main() {
+    matrice_creuse m;
+    remplirMatrice(&m,2,2);
+    afficherMatrice(m);
+    return 0;
+}
