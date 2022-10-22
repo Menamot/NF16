75d7fa88c2fc92c97f4824804c1adac1178e77b5
Menamot <56818152+Menamot@users.noreply.github.com>
2022-10-22 22:23:41 +0200

    补完了2到5函数

--- TP3/TP3.c
+++ TP3/TP3.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "tp3.h"
+#include "TP3.h"
 element *creerElement(int colonne, int valeur) {
     element *nouvelElement = malloc(sizeof(element));
     nouvelElement->col = colonne;
@@ -26,11 +26,11 @@ void remplirMatrice(matrice_creuse *m, int N, int M) {
         for(int j=0;j<M;j++)
         {
             int input;
-            printf("Please enter %d line %d colonne data",i,j);
+//            printf("Please enter %d line %d colonne data",i,j);
             scanf("%d",&input);
             if(input==0)
             {
-                temp=NULL;
+                *temp=NULL;
                 continue;
             }
             else if(input!=0)
@@ -47,39 +47,79 @@ void remplirMatrice(matrice_creuse *m, int N, int M) {
 }
 
 
-void afficherMatrice(matrice_creuse m){
+void afficherMatrice(matrice_creuse m){ //复杂度O(N*M)
+    for (int i = 0; i < m.Nlignes; ++i) {
+        liste_ligne now_node;
+        now_node = m.tab_lignes[i];
+        if (now_node == NULL) { //一行全为0
+            for (int j = 0; j < m.Ncolonnes; ++j) {
+                printf("0 ");
+            }
+        }
+        for (int j = 0; j < m.Ncolonnes; ++j) {
+            if (now_node != NULL){
+                if(now_node->col!=j) { //中间出0
+                    printf("0 ");
+                }
+                else {
+                    printf("%d ",now_node->val);
+                    if((now_node->col==(m.Ncolonnes-2) && now_node->suivant == NULL)) { //一行结尾是0
+                        printf("0 ");
+                    }
+                    now_node=now_node->suivant;
+                }
+            }
 
+        }
+        printf("\n");
+    }
+}
+
+void afficherMatriceListes(matrice_creuse m) { //复杂度O(N*M)
     for (int i = 0; i < m.Nlignes; ++i) {
         liste_ligne now_node;
         now_node = m.tab_lignes[i];
         for (int j = 0; j < m.Ncolonnes; ++j) {
             if (now_node != NULL){
-                printf("%d",now_node->val);
-                now_node=now_node->suivant;
+                    printf("%d/%d ",now_node->val,now_node->col+1); //前者代表值，后者为indice
+                    now_node=now_node->suivant;
             }
         }
+        printf("\n");
     }
-
-}
-
-void afficherMatriceListes(matrice_creuse m) {
-
-    /*Ecrire ici le code de cette fonction*/
-
 }
 
 
-int rechercherValeur(matrice_creuse m, int i, int j) {
-
-    /*Ecrire ici le code de cette fonction*/
-
+int rechercherValeur(matrice_creuse m, int i, int j) { //复杂度O(j)
+    liste_ligne now_node;
+    now_node = m.tab_lignes[i-1];
+    while (1) {
+        if (now_node->col==(j-1)) {return now_node->val;}
+        if (now_node->col>(j-1) || (now_node->col== (j-2) && now_node->suivant== NULL)) {return 0;}
+        now_node = now_node->suivant;
+    }
 }
 
 
-void affecterValeur(matrice_creuse m, int i, int j, int val) {
+void affecterValeur(matrice_creuse m, int i, int j, int val) { //复杂度O(j)
+    liste_ligne now_node;
+    now_node = m.tab_lignes[i-1];
+    for(int k=0;k<j;k++) {
+        if (now_node== NULL && k==0) { //一行全为0
+            m.tab_lignes[i-1] = creerElement(j-1,val);
+            break;
+        }
+        if (now_node->col==(j-1)) {
+            now_node->val=val;
+            break;
+        }
+        if (now_node->suivant== NULL && now_node->col==(j-2)) { //一行结尾为0
+            now_node->suivant = creerElement(j-1,val);
+            break;
+        }
 
-    /*Ecrire ici le code de cette fonction*/
-
+        if (now_node != NULL) {now_node = now_node->suivant;}
+    }
 }
 
 
