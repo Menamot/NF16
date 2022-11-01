0fe8238307312e09433372699e100a1edc5a002b
Menamot <kimimoyan@outlook.com>
2022-11-01 15:12:13 +0100

    第六题初步草稿

--- Code/TP3/main.c
+++ Code/TP3/main.c
@@ -5,7 +5,7 @@
 #include "TP3.h"
 int main() {
     matrice_creuse m[5];
-    int input = 0,k=0,z=0,val;//k是为了当前输入的第几个矩阵，z是存储用户输入的矩阵
+    int input = 0,k=0,z=0,z2=0,val;//k是为了当前输入的第几个矩阵，z是存储用户输入的矩阵
     int N, M;
     printf("-----------------------------------------\n");
     printf("1. Remplir une matrice creuse\n"
